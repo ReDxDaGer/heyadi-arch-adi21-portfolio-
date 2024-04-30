fcc142562a0b0be2b3a476f1fe884be9721858e5
ReDxDaGer <yashmehrotra45@gmail.com>
2024-04-30 21:22:32 +0530

    Updated the Contact images

--- src/portfolio.js
+++ src/portfolio.js
@@ -675,7 +675,7 @@ const publications = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "catachi.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React ,Cloud and Opensource Development.",
   },
